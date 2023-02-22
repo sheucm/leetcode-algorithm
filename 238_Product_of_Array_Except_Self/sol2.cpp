7e41cfcb54ef3aa23a927fbbccf0dfe7d6c819c4
Eric Hsu <st0502123@gmail.com>
2023-02-22 12:27:07 +0800

    complete sol2 for 238

---
+++ 238_Product_of_Array_Except_Self/sol2.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        
+        auto pre = _get_pre(nums);
+        auto suf = _get_suf(nums);
+
+        vector<int> ans;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int pre_v = i ? (pre[i-1]) : 1;
+            int suf_v = (i < (nums.size() - 1))? suf[i+1] : 1;
+            ans.push_back(
+                pre_v * suf_v
+            );
+        }
+        cout << "hello" << endl;
+
+        return ans;
+    }
+private:
+
+    vector<int> _get_pre(vector<int>& nums) {
+        auto pre = vector<int>(nums.size());
+        for (int i = 0; i < nums.size(); i++) {
+            pre[i] = i ? nums[i] * pre[i-1] : nums[i];
+        }
+        return pre;
+    }
+    vector<int> _get_suf(vector<int>& nums) {
+        auto suf = vector<int>(nums.size());
+        for (int i = nums.size() - 1; i >= 0; i--) {
+            suf[i] = (i < (nums.size() - 1))? nums[i] * suf[i+1] : nums[i];
+        }
+        return suf;
+    }
+};
