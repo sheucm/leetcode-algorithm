70cae54c0c9121bb8932b8fbd629b818c2fe1050
Eric Hsu <st0502123@gmail.com>
2023-02-25 20:14:16 +0800

    update sol1

--- 00003_String__Longest_Substring_Without_Repeating_Characters/sol1.cpp
+++ 00003_String__Longest_Substring_Without_Repeating_Characters/sol1.cpp
@@ -10,20 +10,13 @@ public:
         int left = 0;
         for (int i = 0; i < s.size(); i++) {
             char c = s[i];
-
             if (last_idx.count(c)) {
-                // found repeat char
-                int length = i - left;
-                ans = max(ans, length);
-
-                // set new non-repeated left boundary
                 left = max(left, last_idx[c] + 1);
             }
             last_idx[c] = i;
+            ans = max(ans, i - left + 1);
         }
-
-        ans = max(ans, (int)s.size() - left);
-
+        
         return ans;
     }
 };
