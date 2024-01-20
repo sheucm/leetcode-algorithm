5834cff89c07c8acbb013332f7d61ca27cdad294
Eric Hsu <st0502123@gmail.com>
2024-01-20 11:31:53 +0800

    190. Reverse Bits

--- 00190_Binary__Reverse_Bits/sol.py
+++ 00190_Binary__Reverse_Bits/sol.py
@@ -1,5 +1,16 @@
 class Solution:
     def reverseBits(self, n: int) -> int:
-        b = bin(n)[2:].zfill(32)
-        reversed_b = b[::-1]
-        return int(reversed_b, 2)
+
+        # Solution1
+        # b = bin(n)[2:].zfill(32)
+        # reversed_b = b[::-1]
+        # return int(reversed_b, 2)
+
+
+        # Solution2:
+        res = 0
+        for _ in range(32):
+            first_bit = (n & 1)
+            res = (res << 1) | first_bit
+            n >>= 1
+        return res
