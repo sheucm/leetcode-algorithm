7d9c3d9496fcae8554f32662bf55f1e285365633
Eric Hsu <st0502123@gmail.com>
2024-01-21 22:21:25 +0800

    update 70.

--- 00070_DP__Climbing_Stairs/sol.py
+++ 00070_DP__Climbing_Stairs/sol.py
@@ -1,14 +1,29 @@
 class Solution:
+    def __init__(self):
+        self.cache = {}
     def climbStairs(self, n: int) -> int:
-        
-        if n == 1:
-            return 1
-        
-        DP = [0] * (n+1)
-        DP[1] = 1
-        DP[2] = 2
 
-        for i in range(3, n+1):
-            DP[i] = DP[i-1] + DP[i-2]
+
+        ### Solution1: cache
+        ### Time Complexity: O(N)
+        ### Space Compexity: O(N)
+        # if n == 1:
+        #     return 1
+        # if n == 2:
+        #     return 2
+        # if n in self.cache:
+        #     return self.cache[n]
+        # ans = self.climbStairs(n-1) + self.climbStairs(n-2)
+        # self.cache[n] = ans
+        # return ans
+
+
         
-        return DP[n]
+        ### Solution2: DP
+        ### Time Complexity: O(N)
+        ### Space Compexity: O(N)
+        DP = [1] * (n+2)
+        DP[-1] = 0
+        for i in range(n-1, -1, -1):
+            DP[i] = DP[i+1] + DP[i+2]
+        return DP[0]
