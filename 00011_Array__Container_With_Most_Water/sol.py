9f82ed75237f90fd603490bb05078731d16cada5
Eric Hsu <st0502123@gmail.com>
2024-01-21 21:33:21 +0800

    update 11.

--- 00011_Array__Container_With_Most_Water/sol.py
+++ 00011_Array__Container_With_Most_Water/sol.py
@@ -1,19 +1,18 @@
 class Solution:
     def maxArea(self, height: List[int]) -> int:
-        N = len(height)
-
+        
+        ### Time Complexity: O(N)
+        ### Space Complexity: O(1)
         l = 0
-        r = N-1
+        r = len(height)-1
         max_area = 0
         while l < r:
             area = (r-l) * min(height[l], height[r])
             max_area = max(max_area, area)
-
             if height[l] < height[r]:
                 l += 1
             else:
                 r -= 1
-
         return max_area
                 
 
