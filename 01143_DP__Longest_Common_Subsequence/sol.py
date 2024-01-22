a29d8c32cf54e93ff359c5a9254a90c8a7cc7368
Eric Hsu <st0502123@gmail.com>
2024-01-22 08:29:24 +0800

    updat 1143

--- 01143_DP__Longest_Common_Subsequence/sol.py
+++ 01143_DP__Longest_Common_Subsequence/sol.py
@@ -1,10 +1,39 @@
 class Solution:
+
+    def __init__(self):
+        self.cache = {}
+
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
         
+
+        #### Solution1: Cache
+        ### Time Complexity: O(N^2)
+        ### Space Complexity: O(N^2)
+        # if not text1 or not text2:
+        #     return 0
+        # if text1 in self.cache and text2 in self.cache[text1]:
+        #     return self.cache[text1][text2]
+        # if text2 in self.cache and text1 in self.cache[text2]:
+        #     return self.cache[text2][text1]
+        # ans = 0
+        # if text1[0] == text2[0]:
+        #     ans = 1 + self.longestCommonSubsequence(text1[1:], text2[1:])
+        # else:
+        #     ans = max(
+        #         self.longestCommonSubsequence(text1[1:], text2),
+        #         self.longestCommonSubsequence(text1, text2[1:])
+        #     )
+        # self.cache[text1] = self.cache.get(text1, {})
+        # self.cache[text1][text2] = ans
+        # self.cache[text2] = self.cache.get(text2, {})
+        # self.cache[text2][text1] = ans
+        # return ans
+
+
+        ### Solution2: DP
         ### Time Complexity: O(N^2)
         ### Space Complexity: O(N^2)
         DP = [[0] * (len(text2) + 1) for _ in range(len(text1) + 1)]
-
         for i in range(len(text1) - 1, -1, -1):
             for j in range(len(text2) - 1, -1, -1):
                 if text1[i] == text2[j]:
@@ -14,6 +43,5 @@ class Solution:
                         DP[i+1][j],
                         DP[i][j+1]
                     )
-        
         return DP[0][0]
                     
