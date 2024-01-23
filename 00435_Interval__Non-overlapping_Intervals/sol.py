d4bc537f3a4066319875d5851b3d0496c827bb70
Eric Hsu <st0502123@gmail.com>
2024-01-23 21:12:06 +0800

    update 435 code

--- 00435_Interval__Non-overlapping_Intervals/sol.py
+++ 00435_Interval__Non-overlapping_Intervals/sol.py
@@ -1,16 +1,35 @@
 class Solution:
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
+        
+        ### Solution1: Space efficient
+        ### Time Complexity: O(N)
+        ### Space Complexity: O(1)
+        intervals.sort()
+        end = intervals[0][1]
+        ans = 0
+        for itv in intervals[1:]:
+            if itv[0] >= end:
+                end = itv[1]
+            else:
+                ans += 1
+                end = min(end, itv[1])
+        return ans
+        
+
+
+
+        ### Solution2: Will record the answer list
         ### Time Complexity: O(N)
         ### Space Complexity: O(N)
-        intervals.sort(key=lambda i: i[0])
-        output = [intervals[0]]
-        remove_cnt = 0
-        for start, end in intervals[1:]:
-            last_end = output[-1][1]
-            if start < last_end: # overlap
-                remove_cnt += 1
-                if end < last_end:
-                    output[-1] = [start, end]
-            else:
-                output.append([start, end])
-        return remove_cnt
+        # intervals.sort()
+        # output = [intervals[0]]
+        # remove_cnt = 0
+        # for start, end in intervals[1:]:
+        #     last_end = output[-1][1]
+        #     if start < last_end: # overlap
+        #         remove_cnt += 1
+        #         if end < last_end:
+        #             output[-1] = [start, end]
+        #     else:
+        #         output.append([start, end])
+        # return remove_cnt
