57b6409139fee8c7ec02c66b201f5b507a74ba58
Eric Hsu <st0502123@gmail.com>
2024-01-26 10:42:36 +0800

    update 100. code

--- 00100_Tree__Same_Tree/sol.py
+++ 00100_Tree__Same_Tree/sol.py
@@ -6,19 +6,32 @@
 #         self.right = right
 class Solution:
     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
-        ### Solution1: DFS
+        
+        ### Solution3: DFS only
         ### Time Complexity: O(P+Q)
         ### Space Complexity: O(1)
-        def dfs(v):
-            if not v: 
-                return [None]
-            return [str(v.val)] + dfs(v.left) + dfs(v.right)
-        return dfs(p) == dfs(q)
+        def dfs(v, u):
+            if not v and not u: return True
+            if not v or not u or v.val != u.val:  return False
+            if not dfs(v.left, u.left):  return False
+            if not dfs(v.right, u.right): return False
+            return True
+        return dfs(p, q)
 
-
-        ### Solution2: BFS
+        
+        ### Solution1: DFS + output array
         ### Time Complexity: O(P+Q)
-        ### Space Complexity: O(1)
+        ### Space Complexity: O(P+Q)
+        # def dfs(v):
+        #     if not v: 
+        #         return [None]
+        #     return [str(v.val)] + dfs(v.left) + dfs(v.right)
+        # return dfs(p) == dfs(q)
+
+
+        ### Solution2: BFS + output array
+        ### Time Complexity: O(P+Q)
+        ### Space Complexity: O(P+Q)
         # def bfs(v):
         #     q, res = [v], []
         #     while q:
