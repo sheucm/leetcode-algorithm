2df141b4fdedea8405950eb1eb02bf93f4ff1e30
Eric Hsu <st0502123@gmail.com>
2024-01-29 12:00:12 +0800

    update 153. code

--- 00153_Array__Find_Minimum_in_Rotated_Sorted_Array/sol.py
+++ 00153_Array__Find_Minimum_in_Rotated_Sorted_Array/sol.py
@@ -1,22 +1,18 @@
 class Solution:
     def findMin(self, nums: List[int]) -> int:
         
-        ans = min(nums[0], nums[-1])
-
-        start = 0
-        end = len(nums) - 1
-
-        while nums[start] > nums[end]:
-
-            mid = (start + end) // 2
-            if mid == start:
-                break
-            ans = min(ans, nums[mid])
-            if nums[mid] > nums[end]:
-                start = mid
+        
+        l, r = 0, len(nums)-1
+        min_val = min(nums[l], nums[r])
+        while l < r:
+            m = (l+r) // 2
+            if m == l: break
+            min_val = min(min_val, nums[m])
+            if nums[m] > nums[l]:
+                l = m
             else:
-                end = mid
-            
-        return ans
+                r = m
+        return min_val
+        
         
 
